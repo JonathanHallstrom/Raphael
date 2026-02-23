a117e5100f83cae5276105558c04dc8015154326
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-02-23 21:14:34 +0100

    cutnode lmr bench: 13179661

--- src/Raphael/Raphael.cpp
+++ src/Raphael/Raphael.cpp
@@ -418,8 +418,13 @@ i32 Raphael::negamax(
         const i32 new_depth = depth - 1 + extension;
         if (depth >= LMR_DEPTH && move_searched > LMR_FROMMOVE && is_quiet) {
             // late move reduction
-            const i32 red_factor = LMR_TABLE[is_quiet][depth][move_searched] + !is_PV * LMR_NONPV;
+            i32 red_factor = LMR_TABLE[is_quiet][depth][move_searched] + !is_PV * LMR_NONPV;
+            if (cutnode) {
+                red_factor += 128;    
+            }
             const i32 red_depth = min(max(new_depth - red_factor / 128, 1), new_depth);
+            
+
             score = -negamax<false>(
                 red_depth, ply + 1, mvidx + 1, -alpha - 1, -alpha, true, ss + 1, halt
             );
